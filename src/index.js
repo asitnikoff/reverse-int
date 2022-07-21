9d39fa232e072a85fd4752f7fbef742e18346a31
Aleksei Sitnikov <alexsitnikov2003@gmail.com>
2022-07-21 19:13:18 +0300

    feat: realize task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (str) {
+    str = str + "";
+
+    while ((str.length > 0) && ((str[0] == '-') || (str[0] == '0'))) {
+        str = str.slice(1);
+    }
+
+    str = str.split("").reverse().join("");
+
+    return str;
 }
